e96bb2a8a51aff5110354b45ee8af7b8eb551c75
Malena Guallar <malena.guallar@gmail.com>
2024-02-29 21:24:44 +0100

    [refactor] Refactoring scraper into 2 functions

--- api.py
+++ api.py
@@ -1,22 +1,22 @@
 from flask import Flask, request, jsonify
-from scraper import get_all_persons
+from scraper import get_persons
 
 app = Flask(__name__)
 
 # GET request
 @app.route('/')
 def getAll():
-    return jsonify(get_all_persons()), 200
+    return jsonify(get_persons()), 200
 
 # GET request
 @app.route("/get_artist/<index>")
-def getArtist(index, artist_firstname, artist_lastname, field):
-    artistsData = {
-        "index": index,
-        "firstname" : artist_firstname,
-        "lastname": artist_lastname,
-        "field" : field
-    }
+def getArtist(index):
+    # artistsData = {
+    #     "index": index,
+    #     # "firstname" : artist_firstname,
+    #     # "lastname": artist_lastname,
+    #     # "field" : field
+    # }
 
     extra = request.args.get("extra")
     if extra:
--- scraper.py
+++ scraper.py
@@ -1,37 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-}
-
-sourcePage = requests.get('https://en.wikipedia.org/wiki/List_of_Palestinians#Musicians', headers=headers)
-
-soup = BeautifulSoup(sourcePage.text, 'html.parser')
-
-elements = soup.find(class_='wikitable').find_all('tr')[1:]
-
 persons = []
 
+def scraper(url): 
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+    }
+    sourcePage = requests.get(url, headers=headers)
+    soup = BeautifulSoup(sourcePage.text, 'html.parser')
+    elements = soup.find(class_='wikitable').find_all('tr')[1:]
 
-for element in elements:
-    name = element.find('a')
-    if name is not None:
-        fullname = name.text.split(' ')
-        firstname = fullname[0]
-        lastname = ' '.join(fullname[1:])
-        field = element.find('td').find_next_sibling('td')
-        new_person = {"index": 0, "firstname": firstname, "lastname": lastname, "field": field.text}
-        if "Politics" not in field.text and "Academic" not in field.text and "Academia" not in field.text and "Business" not in field.text and "Medicine" not in field.text and "Religion" not in field.text and "Sport" not in field.text:
-            persons.append(new_person)
+
+    for element in elements:
+        name = element.find('a')
+        if name is not None:
+            fullname = name.text.split(' ')
+            firstname = fullname[0]
+            lastname = ' '.join(fullname[1:])
+            field = element.find('td').find_next_sibling('td')
+            new_person = {"id": 0, "firstname": firstname, "lastname": lastname, "field": field.text}
+            if "Politics" not in field.text and "Academic" not in field.text and "Academia" not in field.text and "Business" not in field.text and "Medicine" not in field.text and "Religion" not in field.text and "Sport" not in field.text:
+                persons.append(new_person)
     else:
         name = None
 
-for person in persons:  
-    index = persons.index(person)
-    person["index"] = index
-    
+    for person in persons:  
+        index = persons.index(person)
+        person["id"] = index
 
-def get_all_persons():  
+
+scraper('https://en.wikipedia.org/wiki/List_of_Palestinians#Musicians')
+
+def get_persons():
     return persons
